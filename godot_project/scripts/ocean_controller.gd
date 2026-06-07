4345b0c21ce0fabdb6f9af0410648117e5835578
Kipata <harlow.jerid@gmail.com>
2026-06-06 23:56:10 -0500

    Finalize terrain and water integration features

--- godot_project/scripts/ocean_controller.gd
+++ godot_project/scripts/ocean_controller.gd
@@ -4,11 +4,11 @@ extends Node3D
 @export var water_level: float = 0.0 : set = set_water_level
 
 func _ready():
-    set_water_level(water_level)
+	set_water_level(water_level)
 
 func set_water_level(value: float):
-    water_level = value
-    if is_inside_tree():
-        position.y = water_level
-    else:
-        call_deferred("set_water_level", value)
+	water_level = value
+	if is_inside_tree():
+		position.y = water_level
+	else:
+		call_deferred("set_water_level", value)
