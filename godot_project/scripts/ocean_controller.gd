840a640e83fe7606d7c05ada54cf0a9a4bfd2c8a
Kipata <harlow.jerid@gmail.com>
2026-06-06 20:31:52 -0500

    Add animated displacement to OceanController

--- godot_project/scripts/ocean_controller.gd
+++ godot_project/scripts/ocean_controller.gd
@@ -1,10 +1,26 @@
+@tool
 extends MeshInstance3D
 
 @export var water_level: float = 0.0 : set = set_water_level
+@export var wave_speed: float = 2.0
+@export var wave_height: float = 0.5
+
+var time_passed: float = 0.0
 
 func _ready():
     set_water_level(water_level)
 
+func _process(delta):
+    # Calculate wave offset regardless of editor/runtime
+    time_passed += delta
+    var wave_offset = sin(time_passed * wave_speed) * wave_height
+    
+    # In editor, we want to see the base level. In game, we want the animation.
+    if Engine.is_editor_hint():
+        position.y = water_level
+    else:
+        position.y = water_level + wave_offset
+
 func set_water_level(value: float):
     water_level = value
-    global_position.y = water_level
+    position.y = water_level
